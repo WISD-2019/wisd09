a0c96e76cf22e348d755dd184349dbb81e1c8c9f
梁哲嘉 <twohouse4313@gmail.com>
2019-12-04 17:14:06 +0800

    bb.b2

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,7 @@ Route::get('/home', 'HomeController@index')->name('home');
 Route::gett('/bb/b1', function(){
     return view('bb.b1');
 });
+
+Route::gett('/bb/b2', function(){
+    return view('bb.b2');
+});
